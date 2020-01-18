0254a2f7289cc4a7baf30ab7c42cd6ce9ee63132
Groisman, Amir <amir.groisman@intel.com>
2020-01-18 11:51:25 +0200

    freeze and shutdown process now interacts with user, Thread that checks due time exceptions works great

--- FinalProject/src/application/ControllerProcessMain.java
+++ FinalProject/src/application/ControllerProcessMain.java
@@ -12,6 +12,8 @@ import java.util.concurrent.TimeUnit;
 import com.sun.scenario.effect.Effect.AccelType;
 
 import client.Client;
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -27,6 +29,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import translator.OptionsOfAction;
 import translator.Translator;
 import javafx.event.ActionEvent;
@@ -122,6 +125,9 @@ public class ControllerProcessMain implements Initializable {
 	
 	@FXML
 	private Button director_btn;
+	
+    @FXML
+    public Button logout_btn;
 	
 	 @FXML
 	private TextArea notifications_text;
@@ -852,13 +858,16 @@ public class ControllerProcessMain implements Initializable {
 	
     @FXML
     void logout_click(ActionEvent event) {
-    	try {
-			Client.instance.closeConnection();
-	    	System.exit(0);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//			try {
+//				Client.instance.closeConnection();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			//ScreenController.getScreenController().activate("login");
+//			Main.primaryStage.close();
+//			Main.launch();
+//			//  Platform.runLater( () -> new Main().main().start( new Stage() ) );
     }
     
     public void setRelatedMessages(ArrayList <String> messages, ArrayList<Object> msgData)
--- FinalProject/src/server/Server.java
+++ FinalProject/src/server/Server.java
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 import application.Processes;
 import application.UserProcess;
 import ocsf.server.*;
+import sun.awt.NullComponentPeer;
 
 /**
  * This class overrides some of the methods in the abstract 
@@ -124,17 +125,16 @@ public class Server extends AbstractServer
       System.out.println("ERROR - Could not listen for clients!");
     }
     
-//	Timer timer = new Timer();
-//	
-//	Calendar calendar = Calendar.getInstance();
-//	calendar.set(Calendar.HOUR_OF_DAY, 10);
-//	calendar.set(Calendar.MINUTE, 15);
-//	calendar.set(Calendar.SECOND, 0);
-//	
-//	Date time = calendar.getTime();
-//	//checkDueDateOfProcesses();
-//	
-//	timer.schedule(new checkDueDateOfProcessesTask(), time);
+	Timer timer = new Timer();
+	
+	Calendar calendar = Calendar.getInstance();
+	calendar.set(Calendar.HOUR_OF_DAY, 23);
+	calendar.set(Calendar.MINUTE, 01);
+	calendar.set(Calendar.SECOND, 0);
+	
+	Date time = calendar.getTime();
+	
+	timer.schedule(new checkDueDateOfProcessesTask(), time);
     
   }
   
@@ -172,23 +172,33 @@ public class Server extends AbstractServer
 					Date currentDate = new SimpleDateFormat("yyyy-MM-dd").parse(LocalDate.now().toString());
 					
 					long diff = dueDate.getTime() - currentDate.getTime();
-				    long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+				    int days = (int)(long)(TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
 				    
+				    //hander passed its due date
 				    if(days < 0)
 				    {
-						System.out.println("Send messsage to :"+process.getHandler_id());
-						System.out.println("Send messsage to Supervisor:");
-						System.out.println("Send messsage to Super Super Manager MMM:");
+				    	//Send a notification to handler
+						DBConnector.sendNotification(process.getRequest_id(), "You exceeded due time for this stage",Math.abs(days),DBConnector.getHandlerRole(process.getRequest_id()), "ICM", "");
+				    	//Send a notification to supervisor
+						DBConnector.sendNotification(process.getRequest_id(), DBConnector.getHandlerRole(process.getRequest_id())+" exceeded due time for this stage!",Math.abs(days),"Supervisor", "ICM", "");
+				    	//Send a notification to manager
+						DBConnector.sendNotification(process.getRequest_id(), DBConnector.getHandlerRole(process.getRequest_id())+" exceeded due time for this stage!",Math.abs(days),"Manager", "ICM", "");
 				    }
+				    //a couple of days left for this stage till due time
 				    else
-				    	if(days == 1)
+				    	if(days>=0 && days < 3)
 				    	{
-							System.out.println("Send messsage to :"+process.getHandler_id());
+				    		//Send notification to handler
+							DBConnector.sendNotification(process.getRequest_id(), "You have "+days+" days to complete the stage!",0,DBConnector.getHandlerRole(process.getRequest_id()), "ICM", "");
 				    	}
 					
 				} catch (ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				}
+				catch(NullPointerException ex)
+				{
+					continue;
 				}
 
 				processes.getMyProcess().put(new Integer((int)result.get(i).get(0)), process);
